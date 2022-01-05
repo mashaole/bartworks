937732cc61f19cffb98534dc69bd1d789a9449e7
mashaole <mashaolemogale@gmail.com>
2022-01-06 00:30:01 +0200

    nav bar and footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../Assets/IAmPresenting.png'
+
+var style = {
+    backgroundColor: "#fffff",
+    textAlign: "center",
+    left: "0",
+    bottom: "0",
+    height: "auto",
+    fontSize: "90%",
+    color: "black",
+    padding: "30px",
+    marginRight: "10px",
+    marginTop: "1rem",
+    position: "fixed",
+    width: "100%"
+}
+
+var phantom = {
+    display: 'grid',
+    marginRight: '30px',
+}
+
+function Footer() {
+    return (
+        <div>
+            <div style={phantom} />
+            <div style={style}>
+                Copyright © 2021 'Company'.  All rights reserved.No part of this site may be reproduced without our written permission.
+                <hr />
+                <a href="https://www.iampresenting.com"><img src={logo} loading="lazy" alt="IAmPresenting" />
+                </a>
+            </div>
+        </div >
+    )
+}
+
+export default Footer;

a250746aa36b8555be77c43df60f154fa60bf672
mashaole <mashaolemogale@gmail.com>
2022-01-07 22:04:47 +0200

    artworks gallery

--- src/views/artworks.jsx
+++ src/views/artworks.jsx
@@ -1,30 +1,90 @@
 import React from 'react'
-
-import { Helmet } from 'react-helmet'
 import './artworks.css'
+import { Helmet } from 'react-helmet'
+import { CarouselWrapper } from "react-pretty-carousel"
+
+const artworks = [
+  {
+    id: 1,
+    image: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020429/b%20artworks_2021-12-20_19_11/art/sk5lafxquus7jlghe8dr.jpg',
+    title: 'Jonah and the whale'
+  },
+  {
+    id: 2,
+    image: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020439/b%20artworks_2021-12-20_19_11/art/xt8phyq90v9bgxtlfq5f.png',
+    title: 'Jonah and the whale'
+  },
+  {
+    id: 3,
+    image: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641582964/b%20artworks_2021-12-20_19_11/art/l1xmw8f6miabuymbviqq.jpg',
+    title: 'Jonah and the whale'
+  },
+  {
+    id: 4,
+    image: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020428/b%20artworks_2021-12-20_19_11/art/htlzbr0jgfgcsutwlamz.jpg',
+    title: 'Wrestle'
+  },
+  {
+    id: 5,
+    image: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641582956/b%20artworks_2021-12-20_19_11/art/dfd4iaifsp0tpicvpjym.jpg',
+    title: 'Wrestle'
+  },
+  {
+    id: 6,
+    image: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020428/b%20artworks_2021-12-20_19_11/art/bjq1ut5qhignr9t0jdk3.jpg',
+    title: 'Conquer'
+  },
+  {
+    id: 7,
+    image: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020426/b%20artworks_2021-12-20_19_11/art/hnh1evty6dxutgerogky.jpg',
+    title: 'Conquer'
+  },
+  {
+    id: 8,
+    image: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641583264/b%20artworks_2021-12-20_19_11/art/kvsjfakxha1xbcxqr3zo.jpg',
+    title: 'Untitled'
+  },
+  {
+    id: 9,
+    image: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641583267/b%20artworks_2021-12-20_19_11/art/v9ebkfq7xupm9hyyickr.png',
+    title: 'Untitled'
+  },
+];
 
 const Artworks = () => {
-    return (
-      <div className='container'>
-        <Helmet>
-          <title>Artworks - B Artworks</title>
-          <meta
-            name="description"
-            content="View some of the B Artworks and Aspire to Inspire"
-          />
-          <meta property="og:title" content="Artworks - B Artworks" />
-          <meta
-            property="og:description"
-            content="View some of the B Artworks and Aspire to Inspire."
-          />
-          <meta
-            property="og:image"
-            content="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/7842c45c-d554-4849-91d6-abd1a0c1e9f1/97dc4012-ad95-41fb-8445-20e3c8ad8529?org_if_sml=1"
-          />
-        </Helmet>
-        <h1 className='textt'>Artworks</h1>
-      </div>
-    )
-  }
-  
-  export default Artworks
+  return (
+    <div className='container'>
+      <Helmet>
+        <title>Artworks - B Artworks</title>
+        <meta
+          name="description"
+          content="View some of the B Artworks and Aspire to Inspire"
+        />
+        <meta property="og:title" content="Artworks - B Artworks" />
+        <meta
+          property="og:description"
+          content="View some of the B Artworks and Aspire to Inspire."
+        />
+        <meta
+          property="og:image"
+          content="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/7842c45c-d554-4849-91d6-abd1a0c1e9f1/97dc4012-ad95-41fb-8445-20e3c8ad8529?org_if_sml=1"
+        />
+      </Helmet>
+      <h1 className='textt'>Artworks</h1>
+      <CarouselWrapper items={1} mode="gallery">
+        {artworks.map((item, key) => {
+          return (
+            <a key={item.id}>
+              <img src={item.image} alt={"image1"} />
+              <br />
+              <div style={{ width: "100%", textAlign: "center" }}>
+                {item.title}</div>
+              <br />
+            </a>)
+        })}
+      </CarouselWrapper>
+    </div >
+  )
+}
+
+export default Artworks

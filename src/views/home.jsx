eee255285de0deb4689a1d7721dfcfb2cf1d1e34
mashaole <mashaolemogale@gmail.com>
2022-02-15 18:00:09 +0200

    blindr hero section with split text

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,53 +1,94 @@
-import React, { useState, useEffect,useRef } from 'react'
+import React from 'react'
 import Meta from "../components/meta"
 import './home.css'
+import classNames from 'classnames';
 
-var headings = ['ARTWORKS', 'CLOTHING', 'ARTIST', 'EXHIBITIONS'];
-
-var i = 0;
 var description="B Artworks Gallery Be Artworks and Aspire to Inspire. View Galleries ,Paintings, Artworks, Clothing Designs and Exhibitions."
 var title="Home - B Artworks Gallery"
 var keywords="B Artworks Gallery,B Artworks,Artworks,Art,Visual,Boitumelo Diseko,Nelson,Nelson Makamo,Visual Artist,South African,South African Artist,Galleries,Paintings,Artworks,Design,Clothing,Exhibitions,Contact"
 var image="https://res.cloudinary.com/ddjvdcads/image/upload/v1640020420/b%20artworks_2021-12-20_19_11/exhibit/j9qxwbk3cjtnh7ohcyft.jpg"
 
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.IMAGE_PARTS = 4;
+    this.changeTO = null;
+    this.AUTOCHANGE_TIME = 5000;
+    this.state = { activeSlide: -1, prevSlide: -1, sliderReady: false };
+    
+  }
 
-function Home() {
-  const [route, setRoute] = useState("/artworks")
-  const funRef = useRef(null);
+  componentWillUnmount() {
+    window.clearTimeout(this.changeTO);
+  }
 
-   useEffect(() => {
-    setInterval(function () {
-      const images = [
-        'url("https://res.cloudinary.com/ddjvdcads/image/upload/v1641631083/b%20artworks_2021-12-20_19_11/art/qsoonv9qcwkeawy11ro4.jpg")',
-        'url("https://res.cloudinary.com/ddjvdcads/image/upload/v1641631081/b%20artworks_2021-12-20_19_11/art/epfavozgigmq87ygp2jg.jpg")',
-        'url("https://res.cloudinary.com/ddjvdcads/image/upload/v1640020431/b%20artworks_2021-12-20_19_11/art/viavexxb8mxuoyvd2yra.jpg")',
-        'url("https://res.cloudinary.com/ddjvdcads/image/upload/v1640020441/b%20artworks_2021-12-20_19_11/art/vmpv8yabk910qvzqweeu.jpg")',
-      ]
-      const section = document.querySelector('.Hero')
-      const bg = images[Math.floor(Math.random() * images.length)]
-      section.style.backgroundImage = bg
-      document.querySelector('.text').innerHTML = headings[i];
-      setRoute("/" + headings[i].toLowerCase())
-      if (i == (headings.length - 1)) {
-        i = 0;
-      } else {
-        i++;
-      }
+  componentDidMount() {
+    this.runAutochangeTO();
+    setTimeout(() => {
+      this.setState({ activeSlide: 0, sliderReady: true });
+    }, 0);
+  }
 
-    }, 7000)
-    return () => clearInterval(funRef.current);
-  },[])
+  runAutochangeTO() {
+    this.changeTO = setTimeout(() => {
+      this.changeSlides(1);
+      this.runAutochangeTO();
+    }, this.AUTOCHANGE_TIME);
+  }
 
-  return (
-    <div className='container'>
-      <Meta Description={description} Title={title} Keywords={keywords} Image={image}/>
-      <div className='Hero'>
-        <a href={route}>
-          <h1 className='text'>ARTWORKS</h1>
-        </a>
-      </div>
-    </div>
-  )
-}
+  changeSlides(change) {
+    window.clearTimeout(this.changeTO);
+    const { length } = slides;
+    const prevSlide = this.state.activeSlide;
+    let activeSlide = prevSlide + change;
+    if (activeSlide < 0) activeSlide = length - 1;
+    if (activeSlide >= length) activeSlide = 0;
+    this.setState({ activeSlide, prevSlide });
+  }
 
-export default Home
+  goToPage(page){
+    location.href = page;
+  }
+
+  render() {
+    const { activeSlide, prevSlide, sliderReady } = this.state;
+
+    {<Meta Description={description} Title={title} Keywords={keywords} Image={image}/>}
+    return /*#__PURE__*/(
+      React.createElement("div", { className: classNames('slider', { 's--ready': sliderReady }) }, /*#__PURE__*/
+      React.createElement("div", { className: "slider__slides" },
+      slides?.map((slide, index) => /*#__PURE__*/
+      React.createElement("div", {
+        className: classNames('slider__slide', { 's--active': activeSlide === index, 's--prev': prevSlide === index }),
+        key: slide.text }, /*#__PURE__*/
+
+      React.createElement("div", { className: "slider__slide-content" }, /*#__PURE__*/ /*#__PURE__*/
+      React.createElement("h2", { className: "slider__slide-heading" },
+      slide.text.split('').map(l => /*#__PURE__*/React.createElement("span",{onClick: () => this.goToPage("/"+slide.text.toLowerCase())}, l))),), /*#__PURE__*/
+
+      React.createElement("div", { className: "slider__slide-parts" },
+      [...Array(this.IMAGE_PARTS).fill()].map((x, i) => /*#__PURE__*/
+      React.createElement("div", { className: "slider__slide-part", key: i }, /*#__PURE__*/
+      React.createElement("div", { className: "slider__slide-part-inner", style: { backgroundImage: `url(${slide.img})` } }))))))), /*#__PURE__*/
+      React.createElement("div", { className: "slider__control", onClick: () => this.changeSlides(-1) }), /*#__PURE__*/
+      React.createElement("div", { className: "slider__control slider__control--right", onClick: () => this.changeSlides(1) })
+      ));
+  }}
+
+const slides = [
+{
+  text: 'ARTWORKS',
+  img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641631083/b%20artworks_2021-12-20_19_11/art/qsoonv9qcwkeawy11ro4.jpg' },
+
+{
+  text: 'CLOTHING',
+  img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641631081/b%20artworks_2021-12-20_19_11/art/epfavozgigmq87ygp2jg.jpg' },
+
+{
+  text: 'ARTIST',
+  img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020431/b%20artworks_2021-12-20_19_11/art/viavexxb8mxuoyvd2yra.jpg' },
+
+{
+  text: 'EXHIBITIONS',
+  img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020441/b%20artworks_2021-12-20_19_11/art/vmpv8yabk910qvzqweeu.jpg' }];
+

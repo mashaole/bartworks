5d215afdab4e6a92f5f71e01f48fffa823202a66
mashaole <mashaolemogale@gmail.com>
2022-03-13 14:28:52 +0200

    [refactor][footer] - changed company logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,16 +4,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css'
 
-const logo ="https://res.cloudinary.com/ddjvdcads/image/upload/v1641463445/oembsy727jbrkpo7rfde.png"
+const logo = "https://res.cloudinary.com/ddjvdcads/image/upload/c_scale,h_28/v1647174190/jcxnebzjxzyqmouq2clj.png"
 
 function Footer() {
     return (
-            <div className='footer'>
+        <div className='footer'>
             <a href="https://www.iampresenting.com">
-                <img src={logo}  loading="lazy" style={{width:"70%",height:"70%"}} alt="IAmPresenting" />
-                </a><hr/>
-                Copyright © 2022 B Artworks.  All rights reserved.No part of this site may be reproduced without our written permission.
-            </div>
+                <img src={logo} loading="lazy" style={{ width: "70%", height: "70%" }} alt="IAmPresenting" />
+            </a><hr />
+            Copyright © 2022 B Artworks.  All rights reserved.No part of this site may be reproduced without our written permission.
+        </div>
     )
 }
 

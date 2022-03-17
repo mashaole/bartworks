0a46a1a05c027011fb6e72c5741ee3072aa62c84
mashaole <mashaolemogale@gmail.com>
2022-03-17 18:25:43 +0200

    [refactor] - better logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css'
 
-const logo = "https://res.cloudinary.com/ddjvdcads/image/upload/c_scale,h_40/v1647174190/jcxnebzjxzyqmouq2clj.png"
+const logo = "https://res.cloudinary.com/ddjvdcads/image/upload/v1647468328/cbcm7skswzq8feuypbaf.png"
 
 function Footer() {
     return (
         <div className='footer' onClick={() => window.open("https://www.iampresenting.com", "_blank")} >
             <a href="https://www.iampresenting.com">
-                <img src={logo} loading="lazy" style={{ width: "80%", height: "90%" }} alt="IAmPresenting" />
+                <img src={logo} loading="lazy" style={{ width: "80px", height: "30px" }} alt="IAmPresenting" />
             </a><hr />
             Copyright © 2022 B Artworks.  All rights reserved.No part of this site may be reproduced without our written permission.
         </div>

99d3f82b4013a5bbc301001b26323f2daff40379
mashaole <mashaolemogale@gmail.com>
2022-03-23 21:14:59 +0200

    [refactor][..] - added service worker and vitals

--- src/index.js
+++ src/index.js
@@ -1,53 +1,66 @@
-import React,{useState,useEffect} from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router,Switch,Routes,Route} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import './style.module.css'
-import Home from './views/home.jsx'
-import Artworks from './views/artworks.jsx'
-import Clothing from './views/clothing.jsx'
-import Artist from './views/artist.jsx'
-import Exhibitions from './views/exhibitions'
-import About from './views/about.jsx'
-import Contact from './views/contact.jsx'
-import Navbar from './components/Navbar/Navbar.jsx';
-import Footer from './components/Footer/Footer.jsx'
+import "./style.module.css";
+import Home from "./views/home.jsx";
+import Artworks from "./views/artworks.jsx";
+import Clothing from "./views/clothing.jsx";
+import Artist from "./views/artist.jsx";
+import Exhibitions from "./views/exhibitions";
+import About from "./views/about.jsx";
+import Contact from "./views/contact.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
-function App () {
-
+function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
-    setLoading(false)
-    }, 1500)
-  }, [])
+      setLoading(false);
+    }, 1500);
+  }, []);
 
-  if(loading){
+  if (loading) {
     return (
-      <div style={{height:"100vh",width:"100vw"}} >
-      <img  style={{display:"block", marginTop:"250px",width:"350px",height:"30%"}} loading="lazy" src="https://res.cloudinary.com/ddjvdcads/image/upload/v1640020420/b%20artworks_2021-12-20_19_11/exhibit/tpquxqzqvj8pejyi367q.png" alt="" />
-    </div>
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <img
+          style={{
+            display: "block",
+            marginTop: "250px",
+            width: "350px",
+            height: "30%",
+          }}
+          loading="lazy"
+          src="https://res.cloudinary.com/ddjvdcads/image/upload/v1640020420/b%20artworks_2021-12-20_19_11/exhibit/tpquxqzqvj8pejyi367q.png"
+          alt=""
+        />
+      </div>
     );
-  }
-  else{
-  return (
-    <Router>
+  } else {
+    return (
+      <Router>
         <Navbar />
         <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/artworks" component={Artworks}  />
-        <Route exact path="/clothing" component={Clothing}/>
-        <Route exact path="/artist" component={Artist} />
-        <Route exact path="/exhibitions" component={Exhibitions}/>
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Route path="*" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/artworks" component={Artworks} />
+          <Route exact path="/clothing" component={Clothing} />
+          <Route exact path="/artist" component={Artist} />
+          <Route exact path="/exhibitions" component={Exhibitions} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route path="*" component={Home} />
         </Switch>
-        <Footer/>
-    </Router>
-  )
-}
+        <Footer />
+      </Router>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
+
+serviceWorkerRegistration.register();
+reportWebVitals();
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -3,10 +3,10 @@ import Meta from "../components/meta"
 import './home.css'
 import classNames from 'classnames';
 
-var description="B Artworks Gallery Be Artworks and Aspire to Inspire. View Galleries ,Paintings, Artworks, Clothing Designs and Exhibitions."
-var title="Home - B Artworks Gallery"
-var keywords="B Artworks Gallery,B Artworks,Artworks,Art,Visual,Boitumelo Diseko,Nelson,Nelson Makamo,Visual Artist,South African,South African Artist,Galleries,Paintings,Artworks,Design,Clothing,Exhibitions,Contact"
-var image="https://res.cloudinary.com/ddjvdcads/image/upload/v1640020420/b%20artworks_2021-12-20_19_11/exhibit/j9qxwbk3cjtnh7ohcyft.jpg"
+var description = "B Artworks Gallery Be Artworks and Aspire to Inspire. View Galleries ,Paintings, Artworks, Clothing Designs and Exhibitions."
+var title = "Home - B Artworks Gallery"
+var keywords = "B Artworks Gallery,B Artworks,Artworks,Art,Visual,Boitumelo Diseko,Nelson,Nelson Makamo,Visual Artist,South African,South African Artist,Galleries,Paintings,Artworks,Design,Clothing,Exhibitions,Contact"
+var image = "https://res.cloudinary.com/ddjvdcads/image/upload/v1640020420/b%20artworks_2021-12-20_19_11/exhibit/j9qxwbk3cjtnh7ohcyft.jpg"
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -15,7 +15,7 @@ export default class Home extends React.Component {
     this.changeTO = null;
     this.AUTOCHANGE_TIME = 5000;
     this.state = { activeSlide: -1, prevSlide: -1, sliderReady: false };
-    
+
   }
 
   componentWillUnmount() {
@@ -46,49 +46,55 @@ export default class Home extends React.Component {
     this.setState({ activeSlide, prevSlide });
   }
 
-  goToPage(page){
+  goToPage(page) {
     location.href = page;
   }
 
   render() {
     const { activeSlide, prevSlide, sliderReady } = this.state;
 
-    {<Meta Description={description} Title={title} Keywords={keywords} Image={image}/>}
+    { <Meta Description={description} Title={title} Keywords={keywords} Image={image} /> }
     return /*#__PURE__*/(
       React.createElement("div", { className: classNames('slider', { 's--ready': sliderReady }) }, /*#__PURE__*/
-      React.createElement("div", { className: "slider__slides" },
-      slides?.map((slide, index) => /*#__PURE__*/
-      React.createElement("div", {
-        className: classNames('slider__slide', { 's--active': activeSlide === index, 's--prev': prevSlide === index }),
-        key: slide.text }, /*#__PURE__*/
+        React.createElement("div", { className: "slider__slides" },
+          slides?.map((slide, index) => /*#__PURE__*/
+            React.createElement("div", {
+              className: classNames('slider__slide', { 's--active': activeSlide === index, 's--prev': prevSlide === index }),
+              key: slide.text
+            }, /*#__PURE__*/
 
-      React.createElement("div", { className: "slider__slide-content" }, /*#__PURE__*/ /*#__PURE__*/
-      React.createElement("h2", { className: "slider__slide-heading" },
-      slide.text.split('').map(l => /*#__PURE__*/React.createElement("span",{onClick: () => this.goToPage("/"+slide.text.toLowerCase())}, l))),), /*#__PURE__*/
+              React.createElement("div", { className: "slider__slide-content" }, /*#__PURE__*/ /*#__PURE__*/
+                React.createElement("h2", { className: "slider__slide-heading" },
+                  slide.text.split('').map((l, i) => /*#__PURE__*/React.createElement("span", { onClick: () => this.goToPage("/" + slide.text.toLowerCase()), key: i }, l)))), /*#__PURE__*/
 
-      React.createElement("div", { className: "slider__slide-parts" },
-      [...Array(this.IMAGE_PARTS).fill()].map((x, i) => /*#__PURE__*/
-      React.createElement("div", { className: "slider__slide-part", key: i }, /*#__PURE__*/
-      React.createElement("div", { className: "slider__slide-part-inner", style: { backgroundImage: `url(${slide.img})` } }))))))), /*#__PURE__*/
-      React.createElement("div", { className: "slider__control", onClick: () => this.changeSlides(-1) }), /*#__PURE__*/
-      React.createElement("div", { className: "slider__control slider__control--right", onClick: () => this.changeSlides(1) })
+              React.createElement("div", { className: "slider__slide-parts" },
+                [...Array(this.IMAGE_PARTS).fill()].map((x, i) => /*#__PURE__*/
+                  React.createElement("div", { className: "slider__slide-part", key: i }, /*#__PURE__*/
+                    React.createElement("div", { className: "slider__slide-part-inner", style: { backgroundImage: `url(${slide.img})` } }))))))), /*#__PURE__*/
+        React.createElement("div", { className: "slider__control", onClick: () => this.changeSlides(-1) }), /*#__PURE__*/
+        React.createElement("div", { className: "slider__control slider__control--right", onClick: () => this.changeSlides(1) })
       ));
-  }}
+  }
+}
 
 const slides = [
-{
-  text: 'ARTWORKS',
-  img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641631083/b%20artworks_2021-12-20_19_11/art/qsoonv9qcwkeawy11ro4.jpg' },
+  {
+    text: 'ARTWORKS',
+    img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641631083/b%20artworks_2021-12-20_19_11/art/qsoonv9qcwkeawy11ro4.jpg'
+  },
 
-{
-  text: 'CLOTHING',
-  img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641631081/b%20artworks_2021-12-20_19_11/art/epfavozgigmq87ygp2jg.jpg' },
+  {
+    text: 'CLOTHING',
+    img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1641631081/b%20artworks_2021-12-20_19_11/art/epfavozgigmq87ygp2jg.jpg'
+  },
 
-{
-  text: 'ARTIST',
-  img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020431/b%20artworks_2021-12-20_19_11/art/viavexxb8mxuoyvd2yra.jpg' },
+  {
+    text: 'ARTIST',
+    img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020431/b%20artworks_2021-12-20_19_11/art/viavexxb8mxuoyvd2yra.jpg'
+  },
 
-{
-  text: 'EXHIBITIONS',
-  img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020441/b%20artworks_2021-12-20_19_11/art/vmpv8yabk910qvzqweeu.jpg' }];
+  {
+    text: 'EXHIBITIONS',
+    img: 'https://res.cloudinary.com/ddjvdcads/image/upload/v1640020441/b%20artworks_2021-12-20_19_11/art/vmpv8yabk910qvzqweeu.jpg'
+  }];
 

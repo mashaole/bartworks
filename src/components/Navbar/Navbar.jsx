448be5c7822d951156f3dc738fc40028f61de589
mashaole <mashaolemogale@gmail.com>
2022-05-14 23:12:59 +0200

    firebase hosting test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const navTitles = [
-	{id: 1, title: 'Home', route: '/'},
-	{id: 2, title: 'Artworks', route: '/artworks'},
-	{id: 3, title: 'Clothing', route: '/clothing'},
-	{id: 4, title: 'Artist', route: '/artist'},
-	{id: 5, title: 'Exhibitions', route: '/exhibitions'},
-	{id: 7, title: 'About Us', route: '/about'},
-	{id: 6, title: 'Contact Us', route: '/contact'},
-  ];
+	{ id: 1, title: 'Home', route: '/' },
+	{ id: 2, title: 'Artworks', route: '/artworks' },
+	{ id: 3, title: 'Clothing', route: '/clothing' },
+	{ id: 4, title: 'Artist', route: '/artist' },
+	{ id: 5, title: 'Exhibitions', route: '/exhibitions' },
+	{ id: 7, title: 'About Us', route: '/about' },
+	{ id: 6, title: 'Contact Us', route: '/contact' },
+];
 
 const Navbar = () => {
 	const [open, setOpen] = useState(false);
@@ -27,19 +27,20 @@ const Navbar = () => {
 	return (
 		<nav className="navbar">
 			<Link to="/" className="nav-logo" onClick={closeMenu}>
-				B Artworks.
+				B Artworks
 			</Link>
 			<div onClick={handleClick} className="nav-icon">
 				{open ? <FiX /> : <FiMenu />}
 			</div>
 			<ul className={open ? 'nav-links active' : 'nav-links'}>
-			{navTitles.map((item,key)=>
-               { return(                
-				   <li className="nav-item" key={key}>
-					<a className="nav-link" href={item.route}>
-						{item.title}
-					</a>
-				</li>)})}
+				{navTitles.map((item, key) => {
+					return (
+						<li className="nav-item" key={key}>
+							<a className="nav-link" href={item.route}>
+								{item.title}
+							</a>
+						</li>)
+				})}
 			</ul>
 		</nav>
 	);
